4b07b83131308fc8e0b867479cd9e4a621d381b6
Brijesh Kumar <brijesh.bhayana@indwealth.in>
2021-11-03 01:06:19 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="text-sm lg:text-base font-poppins font-semibold text-secondary flex flex-col justify-center items-center p-4 mb-16 lg:mb-0">
+    </div>
+  );
+};
+
+export default Footer;
