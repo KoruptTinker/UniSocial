f816179bc9facf90eafe418b089dd8541ff4a9b8
Kinshuk <kinshukg31@gmail.com>
2021-11-05 02:08:14 +0530

    Create EditProfile.js

---
+++ src/services/EditProfile.js
@@ -0,0 +1,20 @@
+import firebase from "../firebase/init";
+
+try{
+    const user = firebase.auth().currentUser;
+    if (user != null) {
+    const editProfile = async (
+        updated_biography,
+    ) => {
+            await firebase.firestore.collection("users").doc(user.uid).update({
+            bio: updated_biography 
+            });
+        };
+    }
+    else{
+        console.log("user is null");
+    }
+} catch (error) {
+    console.log(error);
+    return error;
+}
