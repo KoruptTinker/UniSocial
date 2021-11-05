5e7187fc2a24510025d3c077fc444506be724f95
Kinshuk <kinshukg31@gmail.com>
2021-11-05 12:54:29 +0530

    wrapped edit profile

--- src/services/EditProfile.js
+++ src/services/EditProfile.js
@@ -1,20 +1,22 @@
 import firebase from "../firebase/init";
 
-try{
-    const user = firebase.auth().currentUser;
-    if (user != null) {
-    const editProfile = async (
-        updated_biography,
-    ) => {
-            await firebase.firestore.collection("users").doc(user.uid).update({
-            bio: updated_biography 
-            });
-        };
+const ProfileUpdate = (updated_biography) => {
+    try{
+        const user = firebase.auth().currentUser;
+        if (user != null) {
+        const editProfile = async (
+            updated_biography,
+        ) => {
+                await firebase.firestore.collection("users").doc(user.uid).update({
+                bio: updated_biography 
+                });
+            };
+        }
+        else{
+            console.log("user is null");
+        }
+    } catch (error) {
+        console.log(error);
+        return error;
     }
-    else{
-        console.log("user is null");
-    }
-} catch (error) {
-    console.log(error);
-    return error;
 }
