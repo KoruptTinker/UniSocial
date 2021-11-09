2c8d9965ba0f017f55652229bd621c234c398e43
KoruptTinker <pisces1237@gmail.com>
2021-11-10 00:09:30 +0530

    add topics to home

--- src/components/Trends/Trends.jsx
+++ src/components/Trends/Trends.jsx
@@ -1,38 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { getTrending } from "../../services/FetchTopics";
+
 const Trends = () => {
+  const [trending, setTrending] = useState([]);
+
+  useEffect(() => {
+    getTrending().then((data) => {
+      setTrending(data);
+    });
+  }, []);
+
   return (
     <div
       className="bg-white w-full p-5 rounded-lg"
       style={{ height: "max-content" }}>
       <p className="font-poppins font-semibold text-base mb-3">Trends</p>
       <hr />
-      <div className="tags">
-        <p className="py-2 font-noto font-semibold">#programming</p>
-        <p className="font-noto font-medium text-gray-600">213k Tweets</p>
-      </div>
-      <div className="tags">
-        <p className="py-2 font-noto font-semibold">#dev</p>
-        <p className="font-noto font-medium text-gray-600">3k Tweets</p>
-      </div>
-      <div className="tags">
-        <p className="py-2 font-noto font-semibold">#monday</p>
-        <p className="font-noto font-medium text-gray-600">21.3k Tweets</p>
-      </div>
-      <div className="tags">
-        <p className="py-2 font-noto font-semibold">#frontend</p>
-        <p className="font-noto font-medium text-gray-600">33k Tweets</p>
-      </div>
-      <div className="tags">
-        <p className="py-2 font-noto font-semibold">#oneplus</p>
-        <p className="font-noto font-medium text-gray-600">213k Tweets</p>
-      </div>
-      <div className="tags">
-        <p className="py-2 font-noto font-semibold">#programming</p>
-        <p className="font-noto font-medium text-gray-600">25k Tweets</p>
-      </div>
-      <div className="tags">
-        <p className="py-2 font-noto font-semibold">#memes</p>
-        <p className="font-noto font-medium text-gray-600">213k Tweets</p>
-      </div>
+      {trending.map((trend)=>{
+        return(
+        <div className="tags">
+          <p className="py-2 font-noto font-semibold">{trend.topic}</p>
+          <p className="font-noto font-medium text-grey-600">{trend.count} {trend.count === 1 ? "Tweet" : "Tweets"}</p>
+        </div>
+      )})}
     </div>
   );
 };
---
+++ src/services/FetchTopics.js
@@ -0,0 +1,52 @@
+import firebase from "../firebase/init";
+
+const getUnique = (value, index, self) => {
+  return self.indexOf(value) === index;
+}
+
+const removeDuplicates = (topics) => {
+  for(let i = 0; i< topics.length; i++){
+    topics[i] = topics[i].toLowerCase();
+  }
+  const uniqueTopics = topics.filter(getUnique);
+  return uniqueTopics;
+}
+
+const getTopics = async (tweet) => {
+  const topics = tweet.match(/#[a-z]+/gi);
+  const uniqueTopics = removeDuplicates(topics);
+  for(const topic of uniqueTopics){
+    const docRef = await firebase.firestore().collection('topics').doc(topic).get();
+    await firebase.firestore().collection('topics').doc(topic).set({
+      total_count: firebase.firestore.FieldValue.increment(1),
+      monthly_count: firebase.firestore.FieldValue.increment(1),
+      weekly_count: firebase.firestore.FieldValue.increment(1),
+      daily_count: firebase.firestore.FieldValue.increment(1),
+      hourly_count: firebase.firestore.FieldValue.increment(1),
+      score: docRef.exists ? (0.05 * docRef.data().total_count) + (0.10 * docRef.data().monthly_count) + (0.15 * docRef.data().weekly_count) + (0.30 * docRef.data().daily_count) + (0.40 * docRef.data().hourly_count) + 1 : 1
+    },
+    {
+      merge: true
+    });
+  }
+  return uniqueTopics;
+}
+
+const getTrending = async () => {
+  const trendingSnapshot = await firebase.firestore().collection('trending').get();
+  const query = trendingSnapshot.docs;
+  const result = [];
+  for(const topic of query){
+    const temp = {
+      topic: topic.data().topic,
+      count: topic.data().count,
+    }
+    result.push(temp);
+  }
+  return result;
+}
+
+export {
+  getTopics,
+  getTrending
+};
